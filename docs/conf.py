8e9c11ec893fe48d7bd6ea63655ce15c4853cdb6
Filippo Betello <91533406+FilippoBetello@users.noreply.github.com>
2025-06-13 11:59:39 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../'))  # adjust based on your project layout
+sys.path.insert(0, os.path.abspath(os.path.join('..', 'easy_rec')))
 
 # -- Project information -----------------------------------------------------
 
