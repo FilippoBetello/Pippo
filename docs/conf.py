32de3cb53fbd6790c51569c5b16c4ab3499d9667
Filippo Betello <91533406+FilippoBetello@users.noreply.github.com>
2025-06-13 12:01:18 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath(os.path.join('..', 'easy_rec')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'easy_rec')))
 
 # -- Project information -----------------------------------------------------
 
